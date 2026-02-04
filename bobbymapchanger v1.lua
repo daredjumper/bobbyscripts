ee3ff49d8b37de4a68e567f37417bce2ca1e7aa2
bobby <93626895+daredjumper@users.noreply.github.com>
2026-02-03 19:43:18 -0700

    Update bobbymapchanger v1.lua

--- bobbymapchanger v1.lua
+++ bobbymapchanger v1.lua
@@ -40,4 +40,41 @@ else
     end)
 end
 
-loadstring(http.Get("https://raw.githubusercontent.com/daredjumper/bobbyscripts/refs/heads/main/antiadminhouse.lua"))
+local vec11 = Vector3.new(-255.5, 20.5, -255.5)
+local vec22 = Vector3.new(255.5, 20.5, 255.5)
+
+while task.wait(0.05) do
+    for _, char in game.Players:GetChildren() do
+        if char then
+            if char.Character and char.Character:FindFirstChild("HumanoidRootPart") then
+                local partX = char.Character:FindFirstChild("HumanoidRootPart").Position.X
+                local partZ = char.Character:FindFirstChild("HumanoidRootPart").Position.Z
+                local checkX = false
+                local checkZ = false
+                if partX >= vec11.X then
+                    if partX <= vec22.X then
+                        checkX = true
+                    end
+                end
+
+                if partZ >= vec11.Z then
+                    if partZ <= vec22.Z then
+                        checkZ = true
+                    end
+                end
+                if checkX and checkZ then
+                    if char.Character then
+                        local plr = char
+                        if plr and plr.Parent then
+                            if plr.Parent == game.Players then
+                                RunAdonisCommand(":re "..plr.Name)
+                            end
+                        end
+                    end
+                end
+            end
+            
+
+        end
+    end
+end
