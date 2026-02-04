922283f52feeac4ef64c0c01f021ab239474a568
bobby <93626895+daredjumper@users.noreply.github.com>
2026-02-03 19:35:08 -0700

    Refactor anti-admin house script for clarity and safety

--- antiadminhouse.lua
+++ antiadminhouse.lua
@@ -1,42 +1,44 @@
 -- Anti-Admin House v1 --
---    made by bobby    --
+    --    made by bobby    --
 
--- USE THIS WITH A MAP THAT HAS TONS OF SPAWN LOCATIONS --
+    -- USE THIS WITH A MAP THAT HAS TONS OF SPAWN LOCATIONS --
 
-local vec11 = Vector3.new(-255.5, 20.5, -255.5)
-local vec22 = Vector3.new(255.5, 20.5, 255.5)
+    local vec11 = Vector3.new(-255.5, 20.5, -255.5)
+    local vec22 = Vector3.new(255.5, 20.5, 255.5)
 
-while task.wait(0.05) do
-    for _, char in game.Players:GetChildren() do
-        if char then
-            local partX = char.Character:FindFirstChild("HumanoidRootPart").Position.X
-            local partZ = char.Character:FindFirstChild("HumanoidRootPart").Position.Z
+    while task.wait(0.05) do
+        for _, char in game.Players:GetChildren() do
+            if char then
+                if char.Character and char.Character:FindFirstChild("HumanoidRootPart") then
+                    local partX = char.Character:FindFirstChild("HumanoidRootPart").Position.X
+                    local partZ = char.Character:FindFirstChild("HumanoidRootPart").Position.Z
+                    local checkX = false
+                    local checkZ = false
+                    if partX >= vec11.X then
+                        if partX <= vec22.X then
+                            checkX = true
+                        end
+                    end
 
-            local checkX = false
-            local checkZ = false
-            if partX >= vec11.X then
-                if partX <= vec22.X then
-                    checkX = true
-                end
-            end
+                    if partZ >= vec11.Z then
+                        if partZ <= vec22.Z then
+                            checkZ = true
+                        end
+                    end
 
-            if partZ >= vec11.Z then
-                if partZ <= vec22.Z then
-                    checkZ = true
-                end
-            end
-
-            if checkX and checkZ then
-                if char.Character then
-                    local plr = char
-                    if plr and plr.Parent then
-                        if plr.Parent == game.Players then
-                            plr:LoadCharacter()
+                    if checkX and checkZ then
+                        if char.Character then
+                            local plr = char
+                            if plr and plr.Parent then
+                                if plr.Parent == game.Players then
+                                    RunAdonisCommand(":re "..plr.Name)
+                                end
+                            end
                         end
                     end
                 end
-            end
+                
 
+            end
         end
     end
-end
