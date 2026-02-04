ff445b4ea204fa5793193669f0e547b03b43b641
bobby <93626895+daredjumper@users.noreply.github.com>
2026-02-04 15:20:00 -0700

    Update bobbymapchanger v1.lua

--- bobbymapchanger v1.lua
+++ bobbymapchanger v1.lua
@@ -11,24 +11,19 @@ else
     end)
     RunAdonisCommand(":re all")
 
-    local btools = {
-        "Building Tools",
-        "F3X",
-        "Clone",
-        "GameTool",
-        "Hammer"
-    }
+    local btoolsNames = {"Building Tools", "GameTool", "Hammer", "Clone", "Indexing Tool"}
 
-    local function setupAntiF3X(plr)
-        if plr then
-            plr.Backpack.ChildAdded:Connect(function(tool)
-                print(tool)
-                if table.find(btools, tool.Name) then
-                    plr.Backpack:ClearAllChildren()
-                    RunAdonisCommand(":removetools "..plr.Name)
-                end
-            end)
-        end
+    local function setupAntiF3X(playuhDetector)
+        playuhDetector.Backpack.ChildAdded:Connect(function(newitem)
+            if table.find(btoolsNames, newitem.Name) then
+                RunAdonisCommand(":removetools "..playuhDetector.Name)
+            end
+        end)
+        playuhDetector.Character.ChildAdded:Connect(function(newitem)
+            if table.find(btoolsNames, newitem.Name) then
+                RunAdonisCommand(":removetools "..playuhDetector.Name)
+            end
+        end)
     end
 
     for _, player in game.Players:GetChildren() do
@@ -40,41 +35,4 @@ else
     end)
 end
 
-local vec11 = Vector3.new(-255.5, 20.5, -255.5)
-local vec22 = Vector3.new(255.5, 20.5, 255.5)
-
-while task.wait(0.05) do
-    for _, char in game.Players:GetChildren() do
-        if char then
-            if char.Character and char.Character:FindFirstChild("HumanoidRootPart") then
-                local partX = char.Character:FindFirstChild("HumanoidRootPart").Position.X
-                local partZ = char.Character:FindFirstChild("HumanoidRootPart").Position.Z
-                local checkX = false
-                local checkZ = false
-                if partX >= vec11.X then
-                    if partX <= vec22.X then
-                        checkX = true
-                    end
-                end
-
-                if partZ >= vec11.Z then
-                    if partZ <= vec22.Z then
-                        checkZ = true
-                    end
-                end
-                if checkX and checkZ then
-                    if char.Character then
-                        local plr = char
-                        if plr and plr.Parent then
-                            if plr.Parent == game.Players then
-                                RunAdonisCommand(":re "..plr.Name)
-                            end
-                        end
-                    end
-                end
-            end
-            
-
-        end
-    end
-end
+loadstring(http.Get("https://raw.githubusercontent.com/daredjumper/bobbyscripts/refs/heads/main/antiadminhouse.lua"))
